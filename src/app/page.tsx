55507069aedf5ba3e6adb15da43fecd51d4023be
Musa Nazar <musa120509@gmail.com>
2025-08-08 21:07:24 +0100

    First Commit

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import HeroSection from "@/components/HeroSection";
+import ServicesSection from "@/components/ServicesSection";
+import ProcessSection from "@/components/ProcessSection";
+
+export default function Page() {
+  return (
+    <>
+     <HeroSection />
+      <ServicesSection />
+      <ProcessSection />
+    </>
+  )
+}
